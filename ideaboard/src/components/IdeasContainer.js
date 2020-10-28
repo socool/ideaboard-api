8316b3cadcb8aa0f4271b0da93ed458f7780ec74
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2020-10-28 08:06:34 +0700

    fixed CORS

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class IdeasContainer extends Component {
-  render() {
-    return (
-      <div>
-        Ideas
-      </div>
-    )
-  }
+    constructor(props) {
+    super(props)
+    this.state = {
+        ideas: []
+    }
+    }
+      
+    render() {
+        return (
+        <div>
+            Ideas
+        </div>
+        )
+    }
+    componentDidMount() {
+        axios.get('http://localhost:3001/api/v1/ideas.json')
+        .then(response => {
+          console.log(response)
+          this.setState({ideas: response.data})
+        })
+        .catch(error => console.log(error))
+      }
 }
 
 export default IdeasContainer
