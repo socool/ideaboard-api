ec4f3550a5e9dcc1233aaac92f8c25ee8b375899
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2020-10-28 13:25:01 +0700

    add create action

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,12 +1,23 @@
 module Api::V1
     class IdeasController < ApplicationController
       def index
-        @ideas = Idea.all
-        puts "HELLOE"
-        @ideas.each do |idea|
-            puts idea
-        end
+        @ideas = Idea.order("created_at DESC")
         render json: @ideas
       end
+      
+      def create
+        @idea = Idea.create(idea_params)
+        render json: @idea
+      end
+
+      def update
+        @idea = Idea.find(params[:id])
+        @idea.update_attributes(idea_params)
+        render json: @idea
+      end
+    private
+      def idea_params
+        params.require(:idea).permit(:title,:body)
+      end
     end
-  end
+end
