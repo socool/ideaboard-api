6470c648c79b43536a2d5ca0179e0c0e1b7682fe
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2020-10-28 08:37:54 +0700

    add create action

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -13,12 +13,33 @@ class IdeasContainer extends Component {
     render() {
         return (
           <div>
+            <div>
+                <button className="newIdeaButton"
+                    onClick={this.addNewIdea}>
+                    New Idea
+                </button>
+            </div>
             {this.state.ideas.map((idea) => {
               return (<Idea idea={idea} key={idea.id}/>)     
             })}
           </div>
         );
       }
+    addNewIdea = () => {
+        axios.post(
+            'http://localhost:3001/api/v1/ideas',
+            { idea:
+            {
+                title: '',
+                body: ''
+            }
+            }
+        )
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.log(error))
+    }
     componentDidMount() {
         axios.get('http://localhost:3001/api/v1/ideas.json')
         .then(response => {
