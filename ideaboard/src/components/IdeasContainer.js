bceba4245d75a2a35008fef1c6b80b14a7016b29
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2020-10-28 08:16:39 +0700

    fixed styled

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -11,11 +11,18 @@ class IdeasContainer extends Component {
       
     render() {
         return (
-        <div>
-            Ideas
-        </div>
-        )
-    }
+          <div>
+            {this.state.ideas.map((idea) => {
+              return(
+                <div className="tile" key={idea.id} >
+                  <h4>{idea.title}</h4>
+                  <p>{idea.body}</p>
+                </div>
+              )       
+            })}
+          </div>
+        );
+      }
     componentDidMount() {
         axios.get('http://localhost:3001/api/v1/ideas.json')
         .then(response => {
